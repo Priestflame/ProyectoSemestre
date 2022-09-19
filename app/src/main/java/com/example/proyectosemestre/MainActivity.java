a6cb39f0861ffeb0d3015eaf42a9d3b83b06bc41
Ricky <r.rojas78@alumnos.santotomas.cl>
2022-09-18 22:53:14 -0300

    Initial commit

---
+++ app/src/main/java/com/example/proyectosemestre/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.proyectosemestre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Metodo para entrar al Home desde login
+
+    public void ingresarHome(View view) {
+        Intent login = new Intent(this, HomeActivity.class);
+        startActivity(login);
+    }
+    //Metodo para salir al login desde Home
+    // por alguna razón la app se cierra cuando esto está aquí
+    /*
+    public void cerrarSesion(View view) {
+        Intent login = new Intent(this, MainActivity.class);
+        startActivity(login);
+    }*/
+
+}
